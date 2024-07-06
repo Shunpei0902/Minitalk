8adf4c22199caf86466cb9c2881ebb71ab76499d
macbook <macbook@macbooks-MacBook-Air.local>
2024-07-06 18:27:03 +0900

    norm modify

--- minitalk_bonus.h
+++ minitalk_bonus.h
@@ -6,12 +6,12 @@
 /*   By: sasano <shunkotkg0141@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 00:10:15 by sasano            #+#    #+#             */
-/*   Updated: 2023/12/05 00:38:36 by sasano           ###   ########.fr       */
+/*   Updated: 2024/07/06 18:25:00 by sasano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
 
 # include "libft/libft.h"
 # include <signal.h>
